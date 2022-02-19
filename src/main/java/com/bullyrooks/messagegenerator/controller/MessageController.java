df88a79a8f8a6fd68db6ba2d622266a87f873ed7
brian <brian.rook@gmail.com>
2022-02-19 12:42:32 -0700

    logging enabled

--- src/main/java/com/bullyrooks/messagegenerator/controller/MessageController.java
+++ src/main/java/com/bullyrooks/messagegenerator/controller/MessageController.java
@@ -1,12 +1,16 @@
 package com.bullyrooks.messagegenerator.controller;
 
+import com.bullyrooks.messagegenerator.config.LoggingEnabled;
 import com.bullyrooks.messagegenerator.controller.dto.MessageResponseDTO;
 import com.bullyrooks.messagegenerator.service.MessageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@LoggingEnabled
+@Slf4j
 public class MessageController {
     @Autowired
     MessageService messageService;
--- src/main/java/com/bullyrooks/messagegenerator/service/MessageService.java
+++ src/main/java/com/bullyrooks/messagegenerator/service/MessageService.java
@@ -1,9 +1,13 @@
 package com.bullyrooks.messagegenerator.service;
 
+import com.bullyrooks.messagegenerator.config.LoggingEnabled;
 import com.github.javafaker.Faker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@LoggingEnabled
+@Slf4j
 public class MessageService {
     Faker faker = new Faker();
 
